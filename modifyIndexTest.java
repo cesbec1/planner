e9b8fa79022226f84c8ce32d9dda60fbed2b95bc
Charly <cgomez9001@gmail.com>
2021-12-02 23:19:00 -0500

    added junits

--- modifyIndexTest.java
+++ modifyIndexTest.java
@@ -5,18 +5,21 @@ public class modifyIndexTest {
 
     @Test
     void testAddTopic() {
-        
-        
+
+   
     }
 
     @Test
     void testPrint() {
-        
+        modifyIndex test = new modifyIndex();
+        assertTrue(test.toString() != null);    
     }
 
     @Test
     void testRemoveTopic() {
-        
+        modifyIndex test = new modifyIndex();
+        test.addTopic();
+        assertTrue(test.size() < 0);       
     }
 
     @Test
